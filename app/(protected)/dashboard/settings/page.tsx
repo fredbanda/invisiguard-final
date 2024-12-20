2a8bc11e76323adabb643fb4e07fc188c00e30c5
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2024-12-20 21:06:52 +0200

    nextauth_setup

--- app/(protected)/dashboard/settings/page.tsx
+++ app/(protected)/dashboard/settings/page.tsx
@@ -1,6 +1,11 @@
-const SettingsPage = () => {
+import { auth } from "@/auth"
+
+const SettingsPage = async() => {
+  const session = await auth()
   return (
-    <div>SettingsPage</div>
+    <div>
+      {JSON.stringify(session)}
+    </div>
   )
 }
 
---
+++ routes.ts
@@ -0,0 +1,39 @@
+// Where we define all the routes for our app
+/**
+ * These are routes that do not require authentication
+ * Anyone can visit them to get find more about the app
+ * and get a feel for how it works.
+ * @type {string[]}
+ */
+
+export const publicRoutes = [
+    "/",
+    "/about",
+    "/contact",
+]
+
+
+/**
+ * These are routes that are used for authentication
+ * Anyone can visit to authenticate and get a session
+ * will lead to email verification to have an account authenticated.
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+]
+
+/**
+ * These routes are api routes for api authentication
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+
+/**
+ * These are route IS a redirect route
+ * @type {string}
+ */
+
+export const DEFAULT_REDIRECT_ROUTE = "/dashboard"
