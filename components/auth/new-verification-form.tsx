11b398a469c08af2428d4b1edaba497b9ae69d8a
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2024-12-21 19:07:26 +0200

    added

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { RiseLoader } from "react-spinners";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "@/components/form-success";
+import { FormError } from "@/components/form-error";
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (success || error) {
+      return;
+    }
+    if (!token) {
+      setError("Missing token ");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+      .catch((error) => {
+        setError("Something went wrong. Please try again later.");
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Please verify your email"
+      backButtonLabel="Go back to login page"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        {!success && !error && <RiseLoader loading={true} color="#076299" />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+};
